72d1fa75c41f148120a3f036708f20ae208fc976
LinNyiAung <linnyiaung1794@gmail.com>
2024-09-24 21:08:32 +0630

    complete update user functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import { errorHandler } from './error.js';
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
@@ -11,4 +12,4 @@ export const verifyToken = (req, res, next) => {
     req.user = user;
     next();
   });
-}
+};
